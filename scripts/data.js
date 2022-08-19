9b70cdd3c9d8e2a63204cb06ca9e643027764ead
Rai Lu <104931123+railu05@users.noreply.github.com>
2022-08-19 15:19:30 +0400

    Add Additional Meme Photo

--- scripts/data.js
+++ scripts/data.js
@@ -158,7 +158,8 @@ let socmedia =
     };
 
 
-let memelist = ['1.jpeg','2.jpeg','3.jpeg','4.jpeg','5.jpeg','6.jpeg','7.jpeg'];
+let memelist = ['1.jpeg','2.jpeg','3.jpeg','4.jpeg','5.jpeg','6.jpeg','7.jpeg','8.jpeg'
+,'9.jpeg','10.jpeg','11.jpeg','12.jpeg','13.jpeg','14.jpeg','15.jpeg','16.jpeg','17.jpeg','18.jpeg'];
 
 export {pageInfo,whatido, creatorskills,memelist,socmedia,hometown,currentlyBased,personalInformation,jobexppost};
 
